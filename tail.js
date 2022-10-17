769f3bea1381be32182800ebfa72f791360ad409
Tim Quigg <timjquigg@gmail.com>
2022-10-17 17:10:06 -0600

    Added iterative test to tail.js

--- tail.js
+++ tail.js
@@ -12,9 +12,16 @@ const tail = function(input) {
 
 // Tests
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+//tail(words);
+//assertEqual(words.length, 3);
 
-assertEqual(tail([1]).length, 0);
+//const tailWords = words.slice(1);
+//console.log(words, tailWords);
+console.log(`Iteratively testing tail function on [${words}].`);
+for (let i = 0; i < words.length - 1; i ++) {
+  assertEqual(words[i + 1], tail(words)[i]);
+}
 
-assertEqual(tail([]).length, 0);
+//assertEqual(tail([1]).length, 0);
+
+//assertEqual(tail([]).length, 0);
