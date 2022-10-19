897d7ebf22e0beca9393366b721626d64713d749
Tim Quigg <timjquigg@gmail.com>
2022-10-18 20:07:51 -0600

    added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const assertArraysEqual = function(actual, expected) {
+  console.log(
+    eqArrays(actual, expected) ?
+      `✅✅✅ Assertion Passed: ${actual} === ${expected}` :
+      `❌❌❌ Assertion Failed: ${actual} !=== ${expected}`
+  );
+};
+
+const eqArrays = function(arrOne, arrTwo) {
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Tests:
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+
