8eb468c4b1b69b7f65f83bb5e0ec38e20f61279a
Tim Quigg <timjquigg@gmail.com>
2022-10-20 16:13:35 -0600

    added takeUntil.js that will take an array as an input and return a new array with all elements from the input array up until the callback condition is met

---
+++ takeUntil.js
@@ -0,0 +1,53 @@
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  console.log(
+    eqArrays(actual, expected) ?
+      `✅✅✅ Assertion Passed: ${actual} === ${expected}` :
+      `❌❌❌ Assertion Failed: ${actual} !=== ${expected}`
+  );
+};
+
+const takeUntil = function(array, callback) {
+  const newArr = [];
+  for (const item of array) {
+    if (callback(item)) {
+      break;
+    }
+    newArr.push(item);
+  }
+  return newArr;
+};
+
+
+
+//Tests:
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
+
+// Expected Output:
+
+// [ 1, 2, 5, 7, 2 ]
+// --
+// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
