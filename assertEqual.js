c74af05b7d1067527ef7ae3a907bcf44281c407e
Tim Quigg <timjquigg@gmail.com>
2022-10-24 10:25:11 -0600

    Refactored assertEquals and Head to have separate test files and to remove duplicate code. Functions are now imported & exported instead of copied.

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,4 @@ const assertEqual = function(actual, expected) {
   );
 };
 
-// Tests:
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("Hello", "hello");
-assertEqual("Test", "Test");
-assertEqual(1, 4);
+module.exports = assertEqual;
---
+++ test/headTest.js
@@ -0,0 +1,8 @@
+const head = require('../head');
+const assertEqual = require('../assertEqual');
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]), 5);
+assertEqual(head([]), undefined);
+assertEqual(head([5,6,7]), 6);
