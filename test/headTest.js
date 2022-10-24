e54719af6cc6b382f2875a8c079df01c061b15f4
Tim Quigg <timjquigg@gmail.com>
2022-10-24 13:50:13 -0600

    Incorporated Mocha and Chai for testing for the head.js function

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,20 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
-assertEqual(head([5,6,7]), 6);
+describe("#head", () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it('returns \'5\' for [\'5\']', () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it('Returns "Hello" for ["Hello"], "Lighthouse", "labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it('Returns undefined for []', () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
