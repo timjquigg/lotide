08ae37780c16873857091dd491380a073470edee
Tim Quigg <timjquigg@gmail.com>
2022-10-24 10:34:52 -0600

    Refactored eqArrays to move tests to eqArraysTest and remove duplicate code.

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  console.log(
-    actual === expected ?
-      `✅✅✅ Assertion Passed: ${actual} === ${expected}` :
-      `❌❌❌ Assertion Failed: ${actual} !=== ${expected}`
-  );
-};
-
 const eqArrays = function(arrOne, arrTwo) {
 
   if (arrOne.length !== arrTwo.length) {
@@ -31,14 +23,4 @@ const eqArrays = function(arrOne, arrTwo) {
   return true;
 };
 
-// Tests:
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]),true);
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]),false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+module.exports = eqArrays;
