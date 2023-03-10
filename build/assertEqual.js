71ec575fad43bef3e9a5e2bad31f99bcc7a5eed8
Tim Quigg <timjquigg@gmail.com>
2023-03-09 21:38:17 -0700

    Add gitignore

--- build/assertEqual.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.assertEqual = void 0;
-const assertEqual = function (actual, expected) {
-    console.log(actual === expected
-        ? `✅✅✅ Assertion Passed: ${actual} === ${expected}`
-        : `❌❌❌ Assertion Failed: ${actual} !=== ${expected}`);
-};
-exports.assertEqual = assertEqual;
